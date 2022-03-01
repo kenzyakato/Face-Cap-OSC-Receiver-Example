8cfd9008d78aceb9328ba69f11f3d80b32d91234
kenzyakato <87981484+kenzyakato@users.noreply.github.com>
2022-03-01 12:21:22 +0900

    Ready for any VRC users
    VRCSDK3.0 set up and added eyebrow tracking

--- Assets/Blinke.cs
+++ Assets/Blinke.cs
@@ -6,25 +6,45 @@ using OscCore;
 public class Blinke : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float[] Blendersheapes = new float[51];
-    
+    //トラキングされてる顔の指定
     public SkinnedMeshRenderer face;
+    //VRChatのアバターパラメータのアドレスとポートの設定
     string m_Address = "/avatar/parameters/";
     OscClient Client = new OscClient("127.0.0.1", 9000);
-   
+    
     // Update is called once per frame
     void Update()
     {
-        
-        for (int i = 0;i < 51; i++)
+        //目のトラキング値を取得する
+        float L = -face.GetBlendShapeWeight(12) / 100;
+        float R = face.GetBlendShapeWeight(13) / 100;
+        float UP = face.GetBlendShapeWeight(16)/100;
+        float DOWN = -face.GetBlendShapeWeight(10)/100;
+        //目のトラキング値を発送する
+        Client.Send(m_Address + "X", R + L);
+        Client.Send(m_Address + "Y", UP + DOWN);
+        //顔を制御　対応するblendshapkeyを制御する
+        Client.Send(m_Address + 0.ToString(), face.GetBlendShapeWeight(0) / 100);
+        Client.Send(m_Address + 1.ToString(), face.GetBlendShapeWeight(1) / 100);
+        Client.Send(m_Address + 2.ToString(), face.GetBlendShapeWeight(2) / 100);
+        Client.Send(m_Address + 7.ToString(), face.GetBlendShapeWeight(7) / 100);
+        Client.Send(m_Address + 6.ToString(), face.GetBlendShapeWeight(6) / 100);
+        Client.Send(m_Address + 8.ToString(), face.GetBlendShapeWeight(8) / 100);
+        Client.Send(m_Address + 9.ToString(), face.GetBlendShapeWeight(9) / 100);
+        Client.Send(m_Address + 44.ToString(), face.GetBlendShapeWeight(44) / 100);
+        Client.Send(m_Address + 43.ToString(), face.GetBlendShapeWeight(43) / 100);
+        Client.Send(m_Address + 23.ToString(), face.GetBlendShapeWeight(24) / 100);
+        //連続のBlendKeyを制御
+        for (int i = 18;i < 23; i++)
         {
             float n = face.GetBlendShapeWeight(i) / 100;
-            Client.Send(m_Address+i.ToString(), n);
 
+            Client.Send(m_Address + i.ToString(), n);
         }
+
         //var value = propertyOutput.m_MemberIsProperty ? propertyOutput.m_Property.GetValue(propertyOutput.m_SourceComponent) : propertyOutput.m_Field.GetValue(propertyOutput.m_SourceComponent);
-       
-        
+
+
 
     }
     
---
+++ Assets/VRCSDK/SDK3A/Editor/SDK3AImportFix.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace VRCSDK.SDK3.Editor
+{
+    [InitializeOnLoad]
+    public class SDK3AImportFix
+    {
+        private const string packageRuntimePluginsFolder = "Packages/com.vrchat.avatars/Runtime/VRCSDK/Plugins";
+        private const string legacyRuntimePluginsFolder = "Assets/VRCSDK/Plugins/";
+        private const string SDK3_IMPORTS_FIXED = "SDK3AImportsFixed";
+        
+        static SDK3AImportFix()
+        {
+            // Only run once per project
+            string key = Path.Combine(Application.dataPath, SDK3_IMPORTS_FIXED);
+	        
+            if (EditorPrefs.HasKey(key))
+                return;
+
+            EditorPrefs.SetBool(key, true);
+            Run();
+        }
+        
+        public static void Run(){
+            if (Directory.Exists(packageRuntimePluginsFolder))
+            {
+                AssetDatabase.ImportAsset($"{packageRuntimePluginsFolder}/VRCSDK3A.dll",
+                    ImportAssetOptions.ForceSynchronousImport);
+                AssetDatabase.ImportAsset($"{packageRuntimePluginsFolder}/VRCSDK3A-Editor.dll",
+                    ImportAssetOptions.ForceSynchronousImport);
+                AssetDatabase.ImportPackage($"Packages/com.vrchat.avatars/Samples~/AV3 Demo Assets/SDK3A.unitypackage",
+                    false);
+            }
+            else if (Directory.Exists(legacyRuntimePluginsFolder))
+            {
+                AssetDatabase.ImportAsset($"{legacyRuntimePluginsFolder}/VRCSDK3A.dll",
+                    ImportAssetOptions.ForceSynchronousImport);
+                AssetDatabase.ImportAsset($"{legacyRuntimePluginsFolder}/VRCSDK3A-Editor.dll",
+                    ImportAssetOptions.ForceSynchronousImport);
+            }
+        }
+    }
+}
